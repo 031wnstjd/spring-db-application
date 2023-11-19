390bb759c34be00bc081e4de6dee5aee712c3ea7
Junsung <031wnstjd@naver.com>
2023-11-19 23:39:53 +0900

    스프링 트랜잭션 전파5 - 외부 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -89,4 +89,18 @@ public class BasicTxTest {
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer); // 실제 물리 트랜잭션 커밋이 이루어짐 (신규 트랜잭션이기 때문에 실제 커밋을 호출함)
     }
+
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner); // inner는 신규 트랜잭션이 아니므로 논리적 트랜잭션 commit을 호출해도 아무 일이 일어나지 않음
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer); // outer는 신규 트랜잭션이므로 논리적 트랜잭션 rollback을 수행하면 물리적 트랜잭션 rollback이 수행됨
+    }
 }
